4198be1c244ea535d8ce14cc727b3c5dfe428620
tomwsmf <tomhiggins@gmail.com>
2020-10-24 02:55:31 -0500

    More

--- Boxes/Linux/bobbox.sh
+++
@@ -1,1 +0,0 @@
-sudo apt-get install wget mpv ;echo '' >bob.m3u; wget -q -O - http://hourofslack.libsyn.com/rss| tr '\r' '\n' | tr \' \" | sed -n 's/.*url="\([^"]*\)".*/\1/p'|head -n 100 >>bob.m3u; mpv bob.m3u --shuffle --loop-playlist

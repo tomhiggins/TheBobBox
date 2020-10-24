8896b8f9f8dcf09cb61033acca48396ec306076a
tomwsmf <tomhiggins@gmail.com>
2020-10-24 16:53:12 -0500

    Linux

--- Boxes/Linux/bobboxmpv.sh
+++ Boxes/Linux/bobboxmpv.sh
@@ -1,1 +1,1 @@
-echo '' >bob.m3u; wget -q -O - http://hourofslack.libsyn.com/rss| tr '\r' '\n' | tr \' \" | sed -n 's/.*url="\([^"]*\)".*/\1/p'|head -n 100 >bob.m3u; mpv bob.m3u --shuffle --loop-playlist
+echo '' >bob.m3u; wget -q -O - http://hourofslack.libsyn.com/rss| tr '\r' '\n' | tr \' \" | sed -n 's/.*url="\([^"]*\)".*/\1/p'|head -n 100 >bob.m3u; mpv bob.m3u --shuffle --loop-playlist &

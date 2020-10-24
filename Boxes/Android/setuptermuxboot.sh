94e8255227e33d51aa0a8aeddc573b6066909c06
Tom Higgins <tomhiggins@gmail.com>
2020-10-24 15:27:27 -0500

    Update setuptermuxboot.sh

--- Boxes/Android/setuptermuxboot.sh
+++ Boxes/Android/setuptermuxboot.sh
@@ -1,5 +1,4 @@
 mkdir ~/.termux
 mkdir ~/.termux/boot
-cp ~/bobbox.sh  ~/.termux/boot/bobbox.sh
 cd  ~/.termux/boot
 wget  https://raw.githubusercontent.com/tomhiggins/TheBobBox/main/Boxes/Android/startbob

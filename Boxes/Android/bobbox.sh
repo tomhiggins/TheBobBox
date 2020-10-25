60b390eb8ab8e03f83e2f1ab914bbd96ea3bfb4f
tomwsmf <tomhiggins@gmail.com>
2020-10-25 02:37:54 -0500

    .

--- Boxes/Android/bobbox.sh
+++ Boxes/Android/bobbox.sh
@@ -1,1 +1,5 @@
-echo '' >bob.m3u; wget -q -O - http://hourofslack.libsyn.com/rss| tr '\r' '\n' | tr \' \" | sed -n 's/.*url="\([^"]*\)".*/\1/p'|head -n 100 >bob.m3u; mpv bob.m3u --shuffle --loop-playlist
+cd ~
+mkdir bobbox
+cd ~/bobbox
+echo '' >bob.m3u; wget -q -O - http://hourofslack.libsyn.com/rss| tr '\r' '\n' | tr \' \" | sed -n 's/.*url="\([^"]*\)".*/\1/p'|head -n 100 >bob.m3u
+mpv bob.m3u --shuffle --loop-playlist

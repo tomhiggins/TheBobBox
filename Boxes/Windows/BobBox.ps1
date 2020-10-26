bb7140b93a835c94f002019bc4b6d9568962e2e1
Tom Higgins <tomhiggins@gmail.com>
2020-10-26 17:01:11 -0500

    Update BobBox.ps1

--- Boxes/Windows/BobBox.ps1
+++ Boxes/Windows/BobBox.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri 'http://hourofslack.libsyn.com/rss' -Outfile 'feed.xml' ;[xml]$Content = Get-Content feed.xml;$Feed = $Content.rss.channel;ForEach ($msg in $Feed.Item){ echo $msg.guid };& 'C:\Program Files\VideoLAN\VLC\vlc.exe' play.m3u --random --loop
+del bob.m3u;Invoke-WebRequest -Uri 'http://hourofslack.libsyn.com/rss' -Outfile 'feed.xml' ;[xml]$Content = Get-Content feed.xml;$Feed = $Content.rss.channel;ForEach ($msg in $Feed.Item){ echo $msg.enclosure.url | Out-file bob.m3u -Append };& 'C:\Program Files\VideoLAN\VLC\vlc.exe' bob.m3u --random --loop
 
  
